cc5a0b53211414943b5343aec608c024823be302
Sristika Puri <Sristikapuri2057@gmail.com>
2026-06-09 23:52:02 +0545

    updated routes setup

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -4,7 +4,7 @@ import { Router } from "express";
 const router = Router();
 const userController = new UserController();
 
-router.post("/register", (req, res) => userController.createUser(req, res));
-router.post("/login", (req, res) => userController.loginUser(req, res));
+router.post("/register", (req, res) => userController.registerUser(req, res));
+router.post("/login", (req, res) => userController.authenticateUser(req, res));
 
 export default router;
